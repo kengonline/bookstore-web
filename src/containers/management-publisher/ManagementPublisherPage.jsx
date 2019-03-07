4651b09b58c21957099876ea2d84297223b5eee3
kengonline <keang2537@hotmail.com>
2019-03-07 12:19:18 +0700

    Add component TablePagination to page

--- src/containers/management-publisher/ManagementPublisherPage.jsx
+++ src/containers/management-publisher/ManagementPublisherPage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Divider, Table } from "antd";
+import { Divider } from "antd";
 
 // Services
 import { addRouter } from "src/services/router.service";
@@ -8,6 +8,7 @@ import { addRouter } from "src/services/router.service";
 import PublisherCriteria from "src/components/publisher/PublisherCriteria";
 import CustomLayout from "src/components/layout/Layout";
 import CriteriaBox from "src/components/layout/CriteriaBox";
+import TablePagination from "src/components/table/TablePagination";
 
 class ManagementPublisherPage extends Component {
     static configRoute() {
@@ -23,27 +24,22 @@ class ManagementPublisherPage extends Component {
             {
                 title: 'Name',
                 dataIndex: 'name',
-                key: 'name',
-                render: text => <a href="javascript:;">{text}</a>,
+                key: 'name'
             },
             {
-                title: 'Age',
-                dataIndex: 'age',
-                key: 'age',
-            },
-            {
-                title: 'Address',
-                dataIndex: 'address',
-                key: 'address',
+                title: 'Website',
+                dataIndex: 'url',
+                key: 'url',
+                render: text => <a href={text} target="_blank" rel="noopener noreferrer">{text}</a>
             },
             {
                 title: 'Action',
                 key: 'action',
                 render: (text, record) => (
                     <span>
-                        <a href="javascript:;">Invite {record.name}</a>
+                        <a href="#">Invite {record.name}</a>
                         <Divider type="vertical" />
-                        <a href="javascript:;">Delete</a>
+                        <a href="#">Delete</a>
                     </span>
                 ),
             }
@@ -52,23 +48,24 @@ class ManagementPublisherPage extends Component {
         this.state = {
             publisher: [
                 {
-                    key: '1',
-                    name: 'John Brown',
-                    age: 32,
-                    address: 'New York No. 1 Lake Park',
-                    tags: ['nice', 'developer'],
-                }, {
-                    key: '2',
-                    name: 'Jim Green',
-                    age: 42,
-                    address: 'London No. 1 Lake Park',
-                    tags: ['loser'],
-                }, {
-                    key: '3',
-                    name: 'Joe Black',
-                    age: 32,
-                    address: 'Sidney No. 1 Lake Park',
-                    tags: ['cool', 'teacher'],
+                    name: 'Animag',
+                    url: 'http://www.animagshop.com'
+                },
+                {
+                    name: 'Luckpim',
+                    url: 'http://www.luckpim.com'
+                },
+                {
+                    name: 'Dexpress',
+                    url: 'http://shop.dexclub.com'
+                },
+                {
+                    name: 'Zenshu',
+                    url: 'http://www.zenshu.com'
+                },
+                {
+                    name: 'Phoenix Next',
+                    url: 'http://www.phoenixnext.com'
                 }
             ]
         }
@@ -84,7 +81,13 @@ class ManagementPublisherPage extends Component {
                     <PublisherCriteria defaultValue={this.criteria} onChange={this.onCriteriaChange} />
                 </CriteriaBox>
 
-                <Table columns={this.columns} dataSource={publisher} />
+                <TablePagination
+                    tableOptions={{ rowKey: "name" }}
+                    columns={this.columns}
+                    dataSource={publisher}
+                    pagination={{ pageSize: 10, total: 20 }}
+                    onChange={criteria => console.log(criteria)}
+                />
             </CustomLayout>
         );
     }
